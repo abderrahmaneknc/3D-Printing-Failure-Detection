911e5f6db535993fd33183f9b40337a943695049
abderrahmaneknc <a.kennouche@esi-sba.dz>
2026-04-20 19:52:34 +0100

    cmd logs are done

--- app.ts
+++ app.ts
@@ -4,7 +4,8 @@ import printerRoutes from "./src/routes/printer.routes";
 import printJobRoutes from "./src/routes/printJob.routes";
 import partRoutes from "./src/routes/part.routes";
 import tagRoutes from "./src/routes/tag.routes";
-import gCodeCmdRoutes from "./src/routes/gCodeCmd.routes";
+import gCodeCmdRoutes from "./src/routes/gCodeCmd.routes";  
+import commandLogRouter from "./src/routes/cmdLogs.routes";
 const app = express();
 
 app.use(cors());
@@ -15,4 +16,5 @@ app.use("/api/print-jobs", printJobRoutes);
 app.use("/api/parts", partRoutes);
 app.use("/api/tags", tagRoutes);
 app.use("/api/gcode-commands", gCodeCmdRoutes);
+app.use("/api/command-logs", commandLogRouter);
 export default app;
